fc8bfe7f16fc8357d4bd4d6951f249d6f83ef74f
Ali Baran Karataş <kurukluhd.exe@gmail.com>
2026-04-09 16:09:20 +0300

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,14 @@
+USE [SchoolAppDb]
+GO
+
+-- Insert data into Student table
+INSERT INTO [dbo].[Student] ([StudentName], StudentSurname, [StudentEmail])
+VALUES
+('John', 'Smith', 'john.smith@example.com'),
+('Emma', 'Johnson', 'emma.johnson@example.com'),
+('Michael', 'Brown', 'michael.brown@example.com'),
+('Sophia', 'Davis', 'sophia.davis@example.com'),
+('Daniel', 'Wilson', 'daniel.wilson@example.com')
+('Jack', 'Baran', 'karatasalibaran05@gmail.com');
+GO
+
